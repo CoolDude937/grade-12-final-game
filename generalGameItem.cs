5ec6e521fe0a44c978b4c015771ad3d1c424170d
Andy <88058599+CoolDude937@users.noreply.github.com>
2021-11-11 01:57:37 -0500

    final product
    :)

--- generalGameItem.cs
+++ generalGameItem.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework;
@@ -15,79 +15,85 @@ namespace CastleOfPain
     class generalGameItem
     {
         //superclass variables
-        private Texture2D sprite;
-        private Rectangle rect;
-        private Color color;
+        protected Texture2D sprite;
+        protected Rectangle rect;
+        protected Color color;
 
         //constructor
         public generalGameItem(Texture2D aSprite, Rectangle aRect, Color aColor)
         {
             //constructs these 3 variables for use
-            this.setSprite(aSprite);
-            this.setRect(aRect);
-            this.setColor(aColor);
+            setSprite(aSprite);
+            setRect(aRect);
+            setColor(aColor);
         }
 
         //getter for the sprite
         public Texture2D getSprite()
         {
             //returns value of the sprite in this class
-            return this.sprite;
+            return sprite;
         }
 
         //setter for the sprite
         public void setSprite(Texture2D someSprite)
         {
             //assigns the value of the sprite to whatever the user assigns the sprite as
-            this.sprite = someSprite;
+            sprite = someSprite;
         }
 
         //getter for the rectangle
         public Rectangle getRect()
         {
             //returns value of the rectangle in this class
-            return this.rect;
+            return rect;
         }
 
         //setter for the rectangle
         public void setRect(Rectangle someRect)
         {
             //assigns the value of the rectangle to whatever the user assigns the rectangle as
-            this.rect = someRect;
+            rect = someRect;
         }
 
         //getter for the color
         public Color getColor()
         {
             //returns value of the color in this class
-            return this.color;
+            return color;
         }
 
         //setter for the color
         public void setColor(Color someColor)
         {
             //assigns the value of the color to whatever the user assigns the color as
-            this.color = someColor;
+            color = someColor;
         }
 
-                //global hit test
-        public virtual bool hitTest(Rectangle otherRect)
-        {
-            if (this.rect.Intersects(otherRect))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        
         //universal draw method
         public virtual void Draw(SpriteBatch spriteBatch)
         {
             //draws a general game item
             spriteBatch.Draw(sprite, rect, color);
+        }
+
+        //universal hit test
+        public virtual bool hitTest(Rectangle otherRect)
+        {
+            //if this rectangle intersects the other rectangle
+            if (this.rect.Intersects(otherRect))
+            {
+                //return true
+                return true;
+            }
+
+            //else it did not intersect
+            else
+            {
+                //return false
+                return false;
+            }
+
         }
     }
 }
